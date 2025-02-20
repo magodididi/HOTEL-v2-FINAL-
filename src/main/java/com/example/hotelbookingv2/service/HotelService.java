7d726860817f4824a2fe6e1436481f1419061185
magodididi <akutagavik.mine@gmail.com>
2025-02-20 19:34:50 +0300

    Update

--- src/main/java/com/example/hotelbookingv2/service/HotelService.java
+++ src/main/java/com/example/hotelbookingv2/service/HotelService.java
@@ -2,6 +2,7 @@ package com.example.hotelbookingv2.service;
 
 import java.util.List;
 import java.util.Optional;
+import com.example.hotelbookingv2.model.HotelEntity;
 
 public interface HotelService {
     List<HotelEntity> getHotels(String city, String category);
