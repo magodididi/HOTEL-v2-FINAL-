7f8add71467b2ef63cbbfb76bb0dcb10ee774420
magodididi <akutagavik.mine@gmail.com>
2025-03-21 01:34:14 +0300

    L3

--- src/main/java/com/example/hotelbookingv2/cache/LfuCache.java
+++ src/main/java/com/example/hotelbookingv2/cache/LfuCache.java
@@ -9,24 +9,4 @@ public class LfuCache<T> extends LfuCacheBase<T> {
     public LfuCache(int capacity) {
         super(capacity);
     }
-
-    @Override
-    public T get(String id) {
-        return super.get(id);
-    }
-
-    @Override
-    public void put(String id, T value) {
-        super.put(id, value);
-    }
-
-    @Override
-    public void remove(String id) {
-        super.remove(id);
-    }
-
-    @Override
-    public void clear() {
-        super.clear();
-    }
 }
