3408c0f216847cb8e50d5f57896a909335f6e5ab
magodididi <akutagavik.mine@gmail.com>
2025-05-02 10:37:36 +0300

    allow all origins

--- src/main/java/com/example/hotelbookingv2/HotelbookingV2Application.java
+++ src/main/java/com/example/hotelbookingv2/HotelbookingV2Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @EnableCaching
@@ -15,6 +19,18 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class HotelbookingV2Application {
     public static void main(String[] args) {
         SpringApplication.run(HotelbookingV2Application.class, args);
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
 
